a625787e2ca28de3c6e79163e1184f90fbf92335
samilait <sami.p.laitinen@gmail.com>
2019-11-11 19:39:40 +0200

    lisää virheitä

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -28,5 +28,10 @@ public class Main {
         System.out.println("getTilavuus    = " + olutta.getTilavuus());
         System.out.println("paljonkoMahtuu = " + olutta.paljonkoMahtuu());
 
+        System.out.println("Olutgetterit:");
+        System.out.println("getSaldo()     = " + olutta.getSaldo());
+        System.out.println("getTilavuus    = " + olutta.getTilavuus());
+        System.out.println("paljonkoMahtuu = " + olutta.paljonkoMahtuu());
+
     }
 }
